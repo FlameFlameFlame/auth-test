d1c2a90b609202822c69eceb25a9ebf3b6bff130
Timur Akhmadaliev <timur600@gmail.com>
2023-02-21 03:50:40 +0400

    Add AuthIO functionality

--- src/AuthIO.cpp
+++ src/AuthIO.cpp
@@ -4,3 +4,47 @@ AuthIO::AuthIO()
 {
     
 }
+
+void AuthIO::PrintLoginOrLoginCreationPromt(std::ostream &s) const
+{
+    s << "Type 'l' to login or 'c' to create new credentials" << std::endl;
+}
+
+Auth::LoginPassword AuthIO::GetCredentialsFromUser(std::ostream& s, std::istream& i)
+{
+    Auth::LoginPassword lp;
+    s << "login: ";
+    i >> lp.login;
+    s << "password: ";
+    i >> lp.password;
+    return lp;
+}
+
+void AuthIO::PrintLoginError(std::ostream &s) const
+{
+    s << "Incorrect credentials" << std::endl;
+}
+
+std::optional<Auth::LoginPassword> AuthIO::PrintCreateLoginPromt(std::ostream& s, std::istream& i) const
+{
+    Auth::LoginPassword lp;
+    
+    s << "Enter new login: ";
+    i >> lp.login;
+    std::string passwordAttempt1;
+    s << "Enter new password: ";
+    i >> passwordAttempt1;
+    s << "Repeat the password: ";
+    i >> lp.password;
+
+    if (lp.password == passwordAttempt1)
+    {
+        s << "Password mismatch" << std::endl;
+        return std::optional(lp);
+    }
+    else
+    {
+        s << "Success" << std::endl;
+        return std::nullopt;
+    }
+}
--- src/AuthIO.h
+++ src/AuthIO.h
@@ -2,9 +2,19 @@
 
 #include "CommonTypes.h"
 
+#include <optional>
+#include <sstream>
 
 class AuthIO
 {
 public:
     AuthIO();
+
+
+    void PrintLoginOrLoginCreationPromt(std::ostream& s) const;
+
+    Auth::LoginPassword GetCredentialsFromUser(std::ostream& s, std::istream& i);
+    void PrintLoginError(std::ostream& s) const;
+
+    std::optional<Auth::LoginPassword> PrintCreateLoginPromt(std::ostream& s, std::istream& i) const;
 };

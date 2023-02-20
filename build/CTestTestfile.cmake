30d96d4b2d86e19892fcffc73e351763c05970b1
Timur Akhmadaliev <timur600@gmail.com>
2023-02-21 03:49:33 +0400

    Delete build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/timur/Documents/src/auth-test
-# Build directory: /Users/timur/Documents/src/auth-test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

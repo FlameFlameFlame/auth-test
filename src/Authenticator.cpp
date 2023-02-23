64f0002639edf87700c091cca4ac3455bb83d0f4
Timur Akhmadaliev <timur600@gmail.com>
2023-02-24 03:40:00 +0400

    Added new function to Model

--- src/Authenticator.cpp
+++ src/Authenticator.cpp
@@ -11,3 +11,13 @@ Auth::CREATE_LOGING_ATTEMPT_RESULT Authenticator::tryCreateNewLogin(const Auth::
 {
     return CREATE_LOGING_ATTEMPT_RESULT::SUCCESS;
 }
+
+std::optional<std::string> Authenticator::getLoggedInUser() const
+{
+    return !loggedInUser.empty() ? std::optional(loggedInUser) : std::nullopt; 
+}
+
+void Authenticator::logoutCurrentUser()
+{
+    loggedInUser = "";
+}
--- src/Authenticator.h
+++ src/Authenticator.h
@@ -4,11 +4,12 @@
 
 #include <string>
 #include <fstream>
+#include <optional>
 
 class Authenticator {
 private:
     const std::string filename;
-
+    std::string loggedInUser = "";
 public:
     Authenticator(const std::string& i_filename):
         filename(i_filename)
@@ -18,4 +19,9 @@ public:
     
     Auth::LOGIN_ATTEMPT_RESULT tryLogin(const Auth::LoginPassword& loginPassword);
     Auth::CREATE_LOGING_ATTEMPT_RESULT tryCreateNewLogin (const Auth::LoginPassword& loginPassword);
+
+    std::optional<std::string> getLoggedInUser() const;
+    void logoutCurrentUser();
+
+
 };

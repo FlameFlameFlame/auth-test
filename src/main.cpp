a3215ee984e684652006df1ae0070735875126c2
Timur Akhmadaliev <timur600@gmail.com>
2023-02-24 03:41:06 +0400

    Changed main a bit

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "Authenticator.h"
 #include "AuthIO.h"
 
+#include <iostream>
+
 int main()
 {
     Authenticator auth("somefile");
-    AuthIO io;
+    AuthIO io(std::cout, std::cin);
     AuthApp app(auth, io);
     app.runApp();
     return 0;
